b59f2e6e0aecd73d7e465cda96251369b646d0c1
NOHHYUNGBIN <shgudqlsz@naver.com>
2025-04-15 18:05:23 +0900

    리팩토링, 중복제거

--- src/main/java/com/core/springpractice/AppConfig.java
+++ src/main/java/com/core/springpractice/AppConfig.java
@@ -1,6 +1,9 @@
 package com.core.springpractice;
 
+import com.core.springpractice.discount.DiscountPolicy;
 import com.core.springpractice.discount.FixDiscountPolicy;
+import com.core.springpractice.discount.RateDiscountPolicy;
+import com.core.springpractice.member.MemberRepository;
 import com.core.springpractice.member.MemberService;
 import com.core.springpractice.member.MemberServiceImpl;
 import com.core.springpractice.member.MemoryMemberRepository;
@@ -8,11 +11,20 @@ import com.core.springpractice.order.OrderService;
 import com.core.springpractice.order.OrderServiceImpl;
 
 public class AppConfig {
+
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository()) {
-        };
+        return new MemberServiceImpl(getMemberRepository());
     }
+
+    private MemberRepository getMemberRepository() {
+        return new MemoryMemberRepository();
+    }
+
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(getMemberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new RateDiscountPolicy();
     }
  }
