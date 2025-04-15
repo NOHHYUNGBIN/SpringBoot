d846451622171de2ff6c4f06ddf3be471640fcd1
NOHHYUNGBIN <shgudqlsz@naver.com>
2025-04-15 22:37:19 +0900

    singleton

---
+++ src/test/java/com/core/springpractice/beanfind/ApplicationContextInfoTest.java
@@ -0,0 +1,22 @@
+package com.core.springpractice.beanfind;
+
+import com.core.springpractice.AppConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ApplicationContextInfoTest {
+    ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("모든 빈 출력해보기")
+    void findAllBean() {
+        String[] beans = ac.getBeanDefinitionNames();
+        for (String bean : beans) {
+            Object beanInfo = ac.getBean(bean);
+            System.out.println("beanInfo = " + beanInfo);
+        }
+    }
+}
