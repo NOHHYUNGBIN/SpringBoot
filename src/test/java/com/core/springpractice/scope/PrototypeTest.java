0a4b6ba1ce04d8bfbd7300f2a5d6fcb68ec9e342
NOHHYUNGBIN <shgudqlsz@naver.com>
2025-04-17 20:01:46 +0900

    @Scope("prototype")

---
+++ src/test/java/com/core/springpractice/scope/PrototypeTest.java
@@ -0,0 +1,39 @@
+package com.core.springpractice.scope;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PrototypeTest {
+
+    @Test
+    void prototypeBeanFind() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(ProtoTypeBean.class);
+        System.out.println("fine protoTypeBean1");
+        ProtoTypeBean protoTypeBean1 = ac.getBean(ProtoTypeBean.class);
+        System.out.println("fine protoTypeBean2");
+        ProtoTypeBean protoTypeBean2 = ac.getBean(ProtoTypeBean.class);
+        System.out.println("protoTypeBean1 = " + protoTypeBean1);
+        System.out.println("protoTypeBean2 = " + protoTypeBean2);
+
+        assertThat(protoTypeBean1).isNotSameAs(protoTypeBean2);
+        ac.close();
+    }
+
+    @Scope("prototype")
+    static class ProtoTypeBean {
+        @PostConstruct
+        public void connect() {
+            System.out.println("ProtoType init");
+        }
+
+        @PreDestroy
+        public void close() {
+            System.out.println("ProtoType close");
+        }
+    }
+}
