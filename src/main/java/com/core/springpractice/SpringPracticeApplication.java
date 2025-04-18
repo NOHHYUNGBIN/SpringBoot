42e42e9486ffd2c3878bdc063e986bb2c1bdd440
NOHHYUNGBIN <shgudqlsz@naver.com>
2025-04-18 23:37:23 +0900

    EntityManagerFactory

--- src/main/java/com/core/springpractice/SpringPracticeApplication.java
+++ src/main/java/com/core/springpractice/SpringPracticeApplication.java
@@ -1,14 +1,29 @@
 package com.core.springpractice;
 
+import jakarta.persistence.EntityManagerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringPracticeApplication {
-
+public class SpringPracticeApplication implements CommandLineRunner {
+    @Autowired
+    EntityManagerFactory emf;
     public static void main(String[] args) {
 
-        SpringApplication.run(SpringPracticeApplication.class, args);
+//        SpringApplication.run(SpringPracticeApplication.class, args);
+
+        //톰캣이 안뜨도록 설정변경
+        SpringApplication app = new SpringApplication(SpringPracticeApplication.class);
+        app.setWebApplicationType(WebApplicationType.NONE);
+        app.run(args);
     }
 
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("emf = " + emf);
+    }
 }
