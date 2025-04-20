2c196aa660c25f82dcac60a20ec61109ecc6f1e5
NOHHYUNGBIN <shgudqlsz@naver.com>
2025-04-20 21:18:09 +0900

    상세내용 구현

--- src/main/java/com/core/springpractice/BoardController.java
+++ src/main/java/com/core/springpractice/BoardController.java
@@ -4,16 +4,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @Controller
+@RequestMapping("/board")
 public class BoardController {
 
     @Autowired
     BoardService boardService;
 
-    @GetMapping("/board/list")
+    @GetMapping("/read")
+    public String read(Long bno, Model model) {
+        Board board = boardService.read(bno);
+        model.addAttribute("board", board);
+        return "/board/read";
+    }
+
+    @GetMapping("/list")
     public String getList(Model model) {
         List<Board> list = boardService.getList();
         System.out.println("list = " + list);
